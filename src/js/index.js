38a774294c20ad35a08df03f3dc2c48d1e7ed7b3
banyaner <624787620@qq.com>
2016-01-19 13:10:58 +0800

    fix bug

--- src/js/index.js
+++ src/js/index.js
@@ -41,20 +41,20 @@
             case 5:
                 $jsShare.before(el.img3);
                 NewsAppShare.update({
-                    title: 's'
+                    title: '放学别走！小学生向你约战'
                 });
                 break;
             case 6:
             case 7:
                 $jsShare.before(el.img2);
                 NewsAppShare.update({
-                    title: 's'
+                    title: '放学别走！小学生向你约战'
                 });
                 break;
             case 8:
                 $jsShare.before(el.img1);
                 NewsAppShare.update({
-                    title: 's'
+                    title: '放学别走！小学生向你约战'
                 });
                 break;
         }
@@ -62,13 +62,9 @@
 
     //初始化
     function init() {
-
-
         $.getJSON("data/question.json", function (data) {
             data.sort(randomSort);
             var obj = {list: data.slice(0, 8)};//随机抽取8题
-
-            console.log(arr);
             var questionHtml = template('question', obj);
             $('.swiper-wrapper').html(questionHtml);
             var swiper = new Swiper('.swiper-container', {
@@ -76,7 +72,6 @@
                 onSlideNextEnd: function (swiper) {
                     if (swiper.activeIndex == 8) {
                         $('.next-btn').remove();
-                        console.log(rightAns + "right")
                         showResult(rightAns);
                         swiper.lockSwipes();
                     }
@@ -95,7 +90,6 @@
             $('.swiper-container').append(nextHtml);
         });
         $('ul').on('tap', 'li', function () {
-            console.log(this)
             var $this = $(this),
                 $ul = $this.parent(),
                 answer = $ul.attr('data-answer'),
